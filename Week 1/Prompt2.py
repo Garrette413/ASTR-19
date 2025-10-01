8bb85f043960c9611c8eb13330441f38f2e9f998
Garrett Elliott <gdelliot@ucsc.edu>
2025-10-01 14:30:23 -0700

    commit 3

--- Week 1/Prompt2.py
+++ Week 1/Prompt2.py
@@ -1,7 +1,8 @@
-
 a = 3
 b = 5
+c = 8.5
+d = 2.3
 
-print(str(a) + " + " + str(b) + " = " + str(a+b))
-print(str(a) + " - " + str(b) + " = " + str(a-b))
-print(str(a) + " x " + str(b) + " = " + str(a*b))
+print(str(c) + " + " + str(d) + " = " + str(c+d) + " --> " + str(type(c+d)))
+print(str(a) + " - " + str(b) + " = " + str(a-b) + " --> " + str(type(a-b)))
+print(str(a) + " x " + str(c) + " = " + str(a*c) + " --> " + str(type(a*c)))
