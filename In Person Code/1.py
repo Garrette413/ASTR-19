410833ab0933e9dbbb06d660092a7fe77e4f102b
Garrett Elliott <gdelliot@ucsc.edu>
2025-10-28 13:29:29 -0700

    initial commit

---
+++ In Person Code/1.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np
+
+def main():
+    x = 0.0
+
+    for i in range(20):
+        x = i*2 +19
+
+        print(f"the value of x is {x}")
+
+if __name__ == "__main__":
+    main()
